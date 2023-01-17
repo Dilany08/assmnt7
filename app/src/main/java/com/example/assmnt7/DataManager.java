2b43d0cd321bf0e6f135017a1708d65816c41114
edz <118107584+edusuuu@users.noreply.github.com>
2023-01-17 14:08:05 +0800

    Update DataManager.java

--- app/src/main/java/com/example/assmnt7/DataManager.java
+++ app/src/main/java/com/example/assmnt7/DataManager.java
@@ -104,7 +104,7 @@ public class DataManager {
         return myData;
     }
 
-    public void update(String _id, String fname, String course, String section, String midterm, String finals, String tvGrade){
+    public void update(String _id, String fname, String course, String section, String midterm, String finals, String fgrade){
         String query = "UPDATE " + TABLE_STUDENT +
                 TABLE_ROW_FNAME + " = " +"'" + fname + "'" +
                 ", " + "SET " + TABLE_ROW_ID + " = " + "'" + _id + "'" + ", " + TABLE_ROW_COURSE + "=" + "'" + course + "'" + ", " +
@@ -117,7 +117,7 @@ public class DataManager {
                 TABLE_ROW_SECTION + "=" + "'" + section + "'" + ", " +
                 TABLE_ROW_MIDTERM + "=" + "'" + midterm + "'" + ", " +
                 TABLE_ROW_FINALS + "=" + "'" + finals + "'" +
-                TABLE_ROW_FGRADE + "=" + "'" + tvGrade + "'";
+                TABLE_ROW_FGRADE + "=" + "'" + fgrade + "'";
         Log.i("update() = ",query);
         db.execSQL(query);
     }
